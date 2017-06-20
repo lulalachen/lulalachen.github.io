74850a3e95a87b4444a0502899fa9f92f443da6c
Lulala Chen <lulalachen@gmail.com>
2017-06-20 10:09:38 +0800

    loading

---
+++ javascripts/loading.js
@@ -0,0 +1,6 @@
+function loaded() {
+  setTimeout(function() {
+    document.getElementById('loader').className = 'hide';
+    document.getElementById('wrapper').className = 'wrapper show';
+  }, 300)
+}
